e2a1c0bfd62b7cb7c8f36cadafd945dd3c16cfff
waldinho <james.waller1984@gmail.com>
2019-03-12 11:13:18 +1100

    Update scripts.js

--- API/scripts.js
+++ API/scripts.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: "https://api.gousto.co.uk/products/v2.0/categories",
+    url: "",
     dataType: 'json',
     success: function(results){
         console.log(results);
@@ -7,7 +7,7 @@ $.ajax({
 });
 
 $.ajax({
-    url: "https://api.gousto.co.uk/products/v2.0/products?includes[]=categories&includes[]=attributes&sort=position&image_sizes[]=365&i mage_sizes[]=400&period_id=120",
+    url: "",
     dataType: 'json',
     success: function(results){
         console.log(results);
